0ed3412430fc6eae50fcd2133a0b2e52605921b9
Maciej Szefler <ma.szefler@gmail.com>
2022-09-26 05:12:37 +0200

    Calculating median projected separation

--- Properties of Compact Groups of Galaxies/task/main.py
+++ Properties of Compact Groups of Galaxies/task/main.py
@@ -2,29 +2,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
+from astropy.cosmology import FlatLambdaCDM
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import itertools
 
 galaxies = pd.read_csv("galaxies_morphology.tsv", sep="\t")
 isolated = pd.read_csv("isolated_galaxies.tsv", sep="\t")
 df = pd.read_csv("groups.tsv", sep="\t")
+cords = pd.read_csv("galaxies_coordinates.tsv", sep="\t")
 
 df.dropna(inplace=True)
 df.set_index("Group", inplace=True)
+cords.set_index("Group", inplace=True)
 
-mean_galaxies = galaxies.groupby("Group").agg(mean_n = ("n", "mean"), mean_T = ("T", "mean"))
-merged_galaxies = mean_galaxies.merge(df, left_index=True, right_index=True)
+cosmo = FlatLambdaCDM(H0=67.74, Om0=0.3089)
+df["da"] = [cosmo.angular_diameter_distance(x).to(u.kpc) for x in df["z"]]
 
-shapiro_test_mu = stats.shapiro(merged_galaxies.mean_mu).pvalue
-shapiro_test_n = stats.shapiro(merged_galaxies.mean_n).pvalue
-shapiro_test_T = stats.shapiro(merged_galaxies.mean_T).pvalue
-pearson_test_mu_n = stats.pearsonr(merged_galaxies.mean_mu, merged_galaxies.mean_n).pvalue
-pearson_test_mu_T = stats.pearsonr(merged_galaxies.mean_mu, merged_galaxies.mean_T).pvalue
+list_of_medians = []
+iterator_group = df.index.tolist()
+for name in iterator_group:
+    group = cords.loc[name]
+    group.set_index("Name", inplace=True)
+    tuple_of_group = [(group["RA"][x], group["DEC"][x]) for x in range(len(group))]
+    list_of_separations = []
+    for galaxy1, galaxy2 in itertools.combinations(tuple_of_group, 2):
+        p1 = SkyCoord(ra=galaxy1[0] * u.degree, dec=galaxy1[1] * u.degree, frame="fk5")
+        p2 = SkyCoord(ra=galaxy2[0] * u.degree, dec=galaxy2[1] * u.degree, frame="fk5")
+        list_of_separations.append((p1.separation(p2).to(u.radian)*df.loc[name]["da"]).value)
+    series_of_separation = pd.Series(list_of_separations)
+    list_of_medians.append(series_of_separation.median())
 
-print(shapiro_test_mu, shapiro_test_n, shapiro_test_T, pearson_test_mu_n, pearson_test_mu_T)
+df["median"] = list_of_medians
 
-fig, axes = plt.subplots(1, 2)
-ax1, ax2 = axes
-fig.tight_layout(pad=2)
-ax1.scatter(merged_galaxies.mean_n, merged_galaxies.mean_mu)
-ax2.scatter(merged_galaxies.mean_T, merged_galaxies.mean_mu)
-
-plt.show()
+shapiro_test_median = stats.shapiro(df["median"]).pvalue
+shapiro_test_mu = stats.shapiro(df.mean_mu).pvalue
+pearson_test_median_mu = stats.pearsonr(df.mean_mu, df["median"]).pvalue
+print(df.loc["HCG 2"]["median"], shapiro_test_median, shapiro_test_mu, pearson_test_median_mu)
