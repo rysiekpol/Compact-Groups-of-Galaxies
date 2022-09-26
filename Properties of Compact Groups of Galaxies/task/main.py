4f4f10a006734cf93baeb0e7a80f2337437151ee
Maciej Szefler <ma.szefler@gmail.com>
2022-09-26 02:43:08 +0200

    Galaxy morphology and data analysis

--- Properties of Compact Groups of Galaxies/task/main.py
+++ Properties of Compact Groups of Galaxies/task/main.py
@@ -1,18 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
+import numpy as np
 
 galaxies = pd.read_csv("galaxies_morphology.tsv", sep="\t")
 isolated = pd.read_csv("isolated_galaxies.tsv", sep="\t")
-legend_names = ["isolated galaxies", "groups galaxies"]
-#Analyzing data with histogram
-plt.hist([isolated.squeeze(), galaxies["n"]], stacked=True, label=legend_names, edgecolor="black", bins=24, color = ['#ff7f7f', '#7f7fff'])
-plt.ylabel("Count")
-plt.xlabel("n")
-plt.legend()
+df = pd.read_csv("groups.tsv", sep="\t")
+
+df.dropna(inplace=True)
+df.set_index("Group", inplace=True)
+
+mean_galaxies = galaxies.groupby("Group").agg(mean_n = ("n", "mean"), mean_T = ("T", "mean"))
+merged_galaxies = mean_galaxies.merge(df, left_index=True, right_index=True)
+
+shapiro_test_mu = stats.shapiro(merged_galaxies.mean_mu).pvalue
+shapiro_test_n = stats.shapiro(merged_galaxies.mean_n).pvalue
+shapiro_test_T = stats.shapiro(merged_galaxies.mean_T).pvalue
+pearson_test_mu_n = stats.pearsonr(merged_galaxies.mean_mu, merged_galaxies.mean_n).pvalue
+pearson_test_mu_T = stats.pearsonr(merged_galaxies.mean_mu, merged_galaxies.mean_T).pvalue
+
+print(shapiro_test_mu, shapiro_test_n, shapiro_test_T, pearson_test_mu_n, pearson_test_mu_T)
+
+fig, axes = plt.subplots(1, 2)
+ax1, ax2 = axes
+fig.tight_layout(pad=2)
+ax1.scatter(merged_galaxies.mean_n, merged_galaxies.mean_mu)
+ax2.scatter(merged_galaxies.mean_T, merged_galaxies.mean_mu)
+
 plt.show()
-#Analazying dataset with kolmogorov-smirnov test
-galaxies_fraction = galaxies[galaxies.n > 2]["n"].count()/galaxies["n"].count()
-isolated_fraction = isolated[isolated.n > 2]["n"].count()/isolated["n"].count()
-kolmogorov_test = stats.kstest(galaxies["n"], isolated.squeeze(), alternative="two-sided")
-print(galaxies_fraction, isolated_fraction, kolmogorov_test.pvalue)
